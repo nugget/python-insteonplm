2723f4d2f597f7ae24a2a4eed5833e8950ffca6d
David McNett <nugget@macnugget.org>
2017-02-16 13:42:45 -0600

    Current test harness

--- insteonplm/tools.py
+++ insteonplm/tools.py
@@ -9,6 +9,8 @@ import insteonplm
 __all__ = ('console', 'monitor')
 
 
+from .plm import Address, PLMProtocol, Message
+
 @asyncio.coroutine
 def console(loop, log):
     """Connect to receiver and show events as they occur.
@@ -37,7 +39,6 @@ def console(loop, log):
         """Receives event callback from PLM Protocol class."""
         log.info('Callback invoked: %s', message)
 
-
     device = args.device
 
     log.info('Connecting to Insteon PLM at %s', device)
@@ -46,10 +47,9 @@ def console(loop, log):
         device=device, loop=loop)
 
     def async_insteonplm_light_callback(device):
-        log.warn('New Light Device')
-        print(device)
+        log.warn('New Device: %s', device)
 
-    criteria = dict(capability='switch')
+    criteria = dict()
     conn.protocol.add_device_callback(async_insteonplm_light_callback, criteria)
 
     #yield from asyncio.sleep(5, loop=loop)
@@ -60,9 +60,30 @@ def console(loop, log):
     #conn.protocol.product_data_request('15c3ab')
     #yield from asyncio.sleep(10, loop=loop)
 
-    yield from asyncio.sleep(5, loop=loop)
+    yield from asyncio.sleep(10, loop=loop)
 
-    if 1==1:
+    if 1==0:
+        #conn.protocol.send_insteon_extended('4095e6', '2e', '00', '0000000000000000000000000000')
+        # INFO:insteonplm.protocol:INSTEON extended 40.95.E6->39.55.37: cmd1:2e
+        # cmd2:00 flags:15 data:b'0101000020201cff1f0001000000'
+        # d2: 01
+        #
+        conn.protocol.extended_status_request('4095e6')
+        yield from asyncio.sleep(5, loop=loop)
+
+        conn.protocol.update_setlevel('4095e6',127)
+        conn.protocol.update_ramprate('4095e6',27)
+        yield from asyncio.sleep(5, loop=loop)
+
+        conn.protocol.extended_status_request('4095e6')
+        yield from asyncio.sleep(5, loop=loop)
+
+
+    if 1==0:
+        conn.protocol.turn_on('4095e6', ramprate=2)
+        yield from asyncio.sleep(5, loop=loop)
+
+    if 1==0:
         conn.protocol.text_string_request('4095e6')
 
     if 1==0:
@@ -70,19 +91,26 @@ def console(loop, log):
 
     if 1==0:
         conn.protocol.product_data_request('4095e6')
-        conn.protocol.product_data_request('4095e6')
-        conn.protocol.product_data_request('4095e6')
-        conn.protocol.product_data_request('4095e6')
-        conn.protocol.product_data_request('4095e6')
+        conn.protocol.product_data_request('395fa4')
+        conn.protocol.text_string_request('4095e6')
         yield from asyncio.sleep(5, loop=loop)
 
     #conn.protocol._send_raw(binascii.unhexlify('02624095e6150300000000000000ffff000000000000'))
     #yield from asyncio.sleep(5, loop=loop)
 
-    #conn.protocol.get_plm_info()
-
-    #yield from asyncio.sleep(5, loop=loop)
-    #conn.protocol.get_plm_config()
+    if 1==1:
+        print('-- ')
+        conn.protocol.relay_request('395fa4')
+        conn.protocol.relay_request('395ecb')
+        print('-- ')
+        conn.protocol.turn_off('395fa4')
+        conn.protocol.turn_off('395ecb')
+        print('-- ')
+        conn.protocol.turn_on('395fa4', 1)
+        yield from asyncio.sleep(5, loop=loop)
+        print('-- ')
+        conn.protocol.turn_on('395ecb', 1)
+        yield from asyncio.sleep(5, loop=loop)
 
     if 1==0:
         yield from asyncio.sleep(5, loop=loop)
