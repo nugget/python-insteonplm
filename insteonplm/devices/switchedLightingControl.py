ef233de4e70380727ba16d7cfd68a141b5e72309
Tom Harris <tom@harrisnj.net>
2017-12-15 22:52:01 -0500

    Fixed reference to _plm

--- insteonplm/devices/switchedLightingControl.py
+++ insteonplm/devices/switchedLightingControl.py
@@ -14,21 +14,21 @@ class SwitchedLightingControl(DeviceBase):
     def light_on(self, onlevel=0xff):
         if onlevel <= 0:
             return ValueError
-        self.plm.send_standard(self.address.hex, COMMAND_LIGHT_ON_0X11_NONE, onlevel)
+        self._plm.send_standard(self.address.hex, COMMAND_LIGHT_ON_0X11_NONE, onlevel)
 
     def light_on_fast (self, onlevel=0xff):
         if onlevel <= 0:
             return ValueError
-        self.plm.send_standard(self.address.hex, COMMAND_LIGHT_ON_FAST_0X12_NONE, onlevel)
+        self._plm.send_standard(self.address.hex, COMMAND_LIGHT_ON_FAST_0X12_NONE, onlevel)
 
     def light_off(self):
-        self.plm.send_standard(self.address.hex, COMMAND_LIGHT_OFF_0X13_0X00)
+        self._plm.send_standard(self.address.hex, COMMAND_LIGHT_OFF_0X13_0X00)
 
     def light_off_Fast(self):
-        self.plm.send_standard(self.address.hex, COMMAND_LIGHT_OFF_FAST_0X14_0X00)
+        self._plm.send_standard(self.address.hex, COMMAND_LIGHT_OFF_FAST_0X14_0X00)
 
     def light_status_request (self):
-        self.plm.send_standard(self.address.hex, COMMAND_LIGHT_STATUS_REQUEST_0X19_0X00)
+        self._plm.send_standard(self.address.hex, COMMAND_LIGHT_STATUS_REQUEST_0X19_0X00)
 
     def get_operating_flags(self):
         return NotImplemented
