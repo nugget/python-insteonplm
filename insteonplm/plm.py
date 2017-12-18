d759ef37f25cd4e1c8420357b6a51a62850d68d3
Tom Harris <tom@harrisnj.net>
2017-12-17 19:36:09 -0500

    Getting closer

--- insteonplm/plm.py
+++ insteonplm/plm.py
@@ -142,10 +142,9 @@ class PLM(asyncio.Protocol):
         self.log.debug("Starting: send_msg")
         self.log.debug('Sending %d byte message: %s',
                 len(msg.bytes), msg.hex)
-        time.sleep(2)
-        limits = self.transport.get_write_buffer_limits()
-        self.log.info('Write buffer limit is: high: %d  low %d', limits.get('high', -1), limits.get('low', -1))
+        self.transport.pause_reading()
         self.transport.write(msg.bytes)
+        self.transport.resume_writing()
 
         self.log.debug("Ending: send_msg")
 
