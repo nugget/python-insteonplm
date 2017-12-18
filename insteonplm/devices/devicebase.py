19e4d69de527ab6ee0b37fa505039060b31d7e94
Tom Harris <tom@harrisnj.net>
2017-12-18 17:00:18 -0500

    Fixed logging error

--- insteonplm/devices/devicebase.py
+++ insteonplm/devices/devicebase.py
@@ -74,9 +74,9 @@ class DeviceBase(object):
         if callback is None:
             if hasattr(msg, 'cmd1'):
                 if msg.acknak is None:
-                    self.log.debug('No callback found in device %s for message code %02x with cmd1 %02x and cmd2 %02x and acknak %s', msg.code, msg.cmd1, msg.cmd2, 'None')
+                    self.log.debug('No callback found in device %s for message code %02x with cmd1 %02x and cmd2 %02x and acknak %s', self.id, msg.code, msg.cmd1, msg.cmd2, 'None')
                 else:
-                    self.log.debug('No callback found in device %s for message code %02x with cmd1 %02x and cmd2 %02x and acknak %02x', msg.code, msg.cmd1, msg.cmd2, msg.acknak)
+                    self.log.debug('No callback found in device %s for message code %02x with cmd1 %02x and cmd2 %02x and acknak %02x', self.id, msg.code, msg.cmd1, msg.cmd2, msg.acknak)
             else:
                 self.log.debug('No call back found in device %s for message %s', self.id, msg.hex)
         else:
