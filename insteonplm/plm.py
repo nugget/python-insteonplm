1d6032b3e449c3fbfabb359a6ad2c0e69c283382
Tom Harris <tom@harrisnj.net>
2017-12-17 23:23:16 -0500

    Fixed device creation in _handle_assign_to_all_link_group

--- insteonplm/plm.py
+++ insteonplm/plm.py
@@ -200,7 +200,10 @@ class PLM(asyncio.Protocol):
             product_key = msg.target.bytes[2:3]
             self.log.info('Received Device ID with address: %s  cat: 0x%s  subcat: 0x%s  firmware: 0x%s', 
                             msg.address.hex, binascii.hexlify(cat), binascii.hexlify(subcat), binascii.hexlify(product_key))
-            device = self.devices.create_device_from_category(self, msg.address.hex, cat, subcat)
+            device = self.devices.create_device_from_category(self, msg.address.hex, 
+                                                              int.from_bytes(cat, byteorder='big'), 
+                                                              int.from_bytes(subcat, byteorder='big'), 
+                                                              int.from_bytes(product_key, byteorder='big'))
             if device is not None:
                 if isinstance(device, list):
                     for dev in device:
--- tests/test_device.py
+++ tests/test_device.py
@@ -1,3 +1,4 @@
+import binascii
 from insteonplm.devices.device import Device
 from insteonplm.devices.generalController import GeneralController
 from insteonplm.devices.dimmableLightingControl import DimmableLightingControl
@@ -5,6 +6,15 @@ from insteonplm.devices.switchedLightingControl import SwitchedLightingControl
 
 def test_create_device():
     device = Device.create(None, '112233', 0x01, 0x0d, None)
+    print(device.id)
+    assert device.id == '112233'
+    assert isinstance(device, DimmableLightingControl)
+
+def test_create_device_from_bytearray():
+    target = bytearray()
+    target.append(0x01)
+    target.append(0x0d)
+    device = Device.create(None, '112233', int.from_bytes(target[0:1], byteorder='big'), int.from_bytes(target[1:2], byteorder='big'), None)
     print(device.id)
     assert device.id == '112233'
     assert isinstance(device, DimmableLightingControl)
