51748855995fbdba9cc793eef5b9ab84d1fad17f
Tom Harris <tom@harrisnj.net>
2017-12-19 11:44:31 -0500

    More logging of Standard and Extended message creation

--- insteonplm/plm.py
+++ insteonplm/plm.py
@@ -152,6 +152,7 @@ class PLM(asyncio.Protocol):
         self.log.debug("Ending: send_msg")
 
     def send_standard(self, target, commandtuple, cmd2=None, flags=0x00, acknak=None):
+        self.log.debug('Received Standard mesage request with address %s commandtuple cmd1 %02x commandtuple cmd2 %02x override cmd2 %02x flags %02x', target, commandtuple['cmd1'], commandtuple['cmd2'], cmd2, flags)
         if commandtuple.get('cmd1', False):
             cmd1 = commandtuple['cmd1']
             cmd2out = commandtuple['cmd2']
@@ -167,6 +168,7 @@ class PLM(asyncio.Protocol):
         self.send_msg(msg)
 
     def send_extended(self, target, commandtuple, cmd2=None, flags=0x00, acknak=None, **userdata):
+        self.log.debug('Received Extended mesage request with address %s commandtuple cmd1 %02x commandtuple cmd2 %02x override cmd2 %02x flags %02x', target, commandtuple['cmd1'], commandtuple['cmd2'], cmd2, flags)
         if commandtuple.get('cmd1', False):
             cmd1 = commandtuple['cmd1']
             cmd2out = commandtuple['cmd2']
@@ -179,7 +181,7 @@ class PLM(asyncio.Protocol):
                 raise ValueError
 
         
-        self.log.debug('Sending Standard mesage with address %s cmd1 %02x cmd2 %02x flags %02x', target, cmd1, cmd2out, flags)
+        self.log.debug('Sending Extended mesage with address %s cmd1 %02x cmd2 %02x flags %02x', target, cmd1, cmd2out, flags)
         msg = ExtendedSend(target, cmd1, cmd2out,flags,  acknak, **userdata)
         self.send_msg(msg)
 
