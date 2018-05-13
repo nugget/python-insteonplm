9164b8ecea2b6740a3fb93694fe33b4311dee941
Tom Harris <tharris@harrisnj.net>
2018-05-13 15:59:55 -0400

    Fix rare issue where the a device stop responding (#74)
    * Fix issue when ACK / NAK never received from PLM
    
    * Extend wait times
    
    * Extend wait timeouts
    
    * Adjust wait timeout
    
    * Put version to 0.8.6 for testing
    
    * Check ACK/NAK matches msg sent
    
    * Increase timeout to 15 seconds
    
    * Increase timeout to 30 sec

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,7 +1,17 @@
 """Test insteonplm.evices module."""
+import asyncio
+import logging
+
+from insteonplm.constants import (COMMAND_LIGHT_OFF_0X13_0X00,
+                                  COMMAND_LIGHT_ON_0X11_NONE,
+                                  MESSAGE_ACK)
+from insteonplm.messages.standardSend import StandardSend
 import insteonplm.devices
 from insteonplm.devices.dimmableLightingControl import DimmableLightingControl
-from .mockPLM import MockPLM
+from tests.mockPLM import MockPLM
+from insteonplm.devices import DIRECT_ACK_WAIT_TIMEOUT
+
+_LOGGING = logging.getLogger(__name__)
 
 
 def test_create_device():
@@ -23,3 +33,43 @@ def test_create_device_from_bytearray():
                                        None)
     assert device.id == '112233'
     assert isinstance(device, DimmableLightingControl)
+
+
+def test_send_msg():
+
+    @asyncio.coroutine
+    def run_test(loop):
+        mockPLM = MockPLM(loop)
+        address = '1a2b3c'
+        device = insteonplm.devices.create(mockPLM, address, 0x01, 0x0d, 0x44)
+        mockPLM.devices[address] = device
+
+        # Send the ON command. This should be sent directly to the PLM
+        device.states[0x01].on()
+        yield from asyncio.sleep(.1, loop=loop)
+
+        # Send the OFF command. This should wait in queue until the
+        # Direct ACK timeout
+        device.states[0x01].off()
+        yield from asyncio.sleep(.1, loop=loop)
+
+        # ACK the ON command
+        msgreceived = StandardSend(address, COMMAND_LIGHT_ON_0X11_NONE,
+                                   cmd2=0xff, flags=0x00, acknak=MESSAGE_ACK)
+        mockPLM.message_received(msgreceived)
+        asyncio.sleep(.1, loop=loop)
+
+        _LOGGING.debug('Assert that the ON command is the command in the PLM')
+        assert mockPLM.sentmessage == StandardSend(
+            address, COMMAND_LIGHT_ON_0X11_NONE, cmd2=0xff, flags=0x00).hex
+
+        # Sleep until the Direct ACK time out should expire
+        yield from asyncio.sleep(DIRECT_ACK_WAIT_TIMEOUT + .2,
+                                 loop=loop)
+
+        # Confirm that the OFF command made it to the PLM
+        assert mockPLM.sentmessage == StandardSend(
+            address, COMMAND_LIGHT_OFF_0X13_0X00).hex
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_test(loop))
--- tests/test_plm.py
+++ tests/test_plm.py
@@ -1,12 +1,44 @@
 """Test the INSTEON PLM device."""
 import asyncio
+import async_timeout
 import logging
 import binascii
 
 import insteonplm
-from insteonplm.constants import MESSAGE_FLAG_BROADCAST_0X80
+from insteonplm.constants import (COMMAND_LIGHT_ON_0X11_NONE,
+                                  COMMAND_LIGHT_OFF_0X13_0X00,
+                                  MESSAGE_FLAG_BROADCAST_0X80,
+                                  COMMAND_ID_REQUEST_0X10_0X00,
+                                  COMMAND_LIGHT_STATUS_REQUEST_0X19_0X00,
+                                  MESSAGE_NAK,
+                                  MESSAGE_ACK)
 from insteonplm.plm import PLM
 from insteonplm.address import Address
+from insteonplm.messages.standardSend import StandardSend
+from insteonplm.messages.standardReceive import StandardReceive
+from insteonplm.messages.getIMInfo import GetImInfo
+from insteonplm.messages.getFirstAllLinkRecord import GetFirstAllLinkRecord
+from insteonplm.messages.getNextAllLinkRecord import GetNextAllLinkRecord
+from insteonplm.messages.allLinkRecordResponse import AllLinkRecordResponse
+
+_LOGGER = logging.getLogger()
+SEND_MSG_WAIT = 1.1
+SEND_MSG_ACKNAK_WAIT = .2
+DIRECT_ACK_WAIT_TIMEOUT = 3.1
+RECV_MSG_WAIT = .1
+
+
+@asyncio.coroutine
+def wait_for_plm_command(plm, cmd, loop):
+    try:
+        with async_timeout.timeout(10, loop=loop):
+            while not plm.transport.lastmessage == cmd.hex:
+                yield from asyncio.sleep(.1, loop=loop)
+            _LOGGER.info('Expected message sent %s', cmd)
+            return True
+    except asyncio.TimeoutError:
+        _LOGGER.error('Expected message not sent %s', cmd)
+        return False
 
 
 # pylint: disable=too-few-public-methods
@@ -15,7 +47,6 @@ class MockConnection():
 
     def __init__(self):
         """Instantiate the Connection object."""
-        self.log = logging.getLogger(__name__)
         self.loop = None
         self.protocol = None
         self.transport = None
@@ -36,7 +67,6 @@ class MockConnection():
 
             def __init__(self):
                 """Initialize the mock Serial class."""
-                self.log = logging.getLogger(__name__)
                 self.write_timeout = 0
                 self.timeout = 0
 
@@ -45,7 +75,6 @@ class MockConnection():
 
             def __init__(self):
                 """Initialize the mock Transport class."""
-                self.log = logging.getLogger(__name__)
                 self.serial = Serial()
                 self.lastmessage = None
                 self._mock_buffer_size = 128
@@ -60,124 +89,174 @@ class MockConnection():
 
             def write(self, data):
                 """Mock write data to the Connection."""
-                self.lastmessage = data
-                self.log.info('Message sent: %s',
-                              binascii.hexlify(self.lastmessage))
+                self.lastmessage = binascii.hexlify(data).decode()
+                _LOGGER.info('Message sent: %s', self.lastmessage)
 
         conn.transport = Transport()
         return conn
 
 
 @asyncio.coroutine
-def do_plm(loop, log, devicelist):
+def do_plm(loop):
     """Asyncio coroutine to test the PLM."""
-    log.info('Connecting to Insteon PLM')
+    _LOGGER.info('Connecting to Insteon PLM')
 
     # pylint: disable=not-an-iterable
     conn = yield from MockConnection.create(loop=loop)
 
     def async_insteonplm_light_callback(device):
         """Log that our new device callback worked."""
-        log.warn('New Device: %s %02x %02x %s, %s', device.id, device.cat,
-                 device.subcat, device.description, device.model)
+        _LOGGER.warn('New Device: %s %02x %02x %s, %s', device.id, device.cat,
+                     device.subcat, device.description, device.model)
 
     def async_light_on_level_callback(device_id, state, value):
         """Callback to catch changes to light value."""
-        log.info('Device %s state %s value is changed to %02x',
-                 device_id, state, value)
+        _LOGGER.info('Device %s state %s value is changed to %02x',
+                     device_id, state, value)
 
     conn.protocol.add_device_callback(async_insteonplm_light_callback)
 
     plm = conn.protocol
     plm.connection_made(conn.transport)
 
-    log.info('Replying with IM Info')
-    log.info('_____________________')
+    _LOGGER.info('Replying with IM Info')
+    _LOGGER.info('_____________________')
+    cmd_sent = yield from wait_for_plm_command(plm, GetImInfo(), loop)
+    if not cmd_sent:
+        assert False
     msg = insteonplm.messages.getIMInfo.GetImInfo(address='1a2b3c', cat=0x03,
                                                   subcat=0x20, firmware=0x00,
-                                                  acknak=0x06)
+                                                  acknak=MESSAGE_ACK)
     plm.data_received(msg.bytes)
-    yield from asyncio.sleep(.1)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
     assert plm.address == Address('1a2b3c')
     assert plm.cat == 0x03
     assert plm.subcat == 0x20
     assert plm.product_key == 0x00
 
-    log.info('Replying with an All-Link Record')
-    log.info('________________________________')
-    msg = insteonplm.messages.allLinkRecordResponse.AllLinkRecordResponse(
+    _LOGGER.info('Replying with an All-Link Record')
+    _LOGGER.info('________________________________')
+    cmd_sent = yield from wait_for_plm_command(plm, GetFirstAllLinkRecord(),
+                                               loop)
+    if not cmd_sent:
+        assert False
+    msg = GetFirstAllLinkRecord(MESSAGE_ACK)
+    plm.data_received(msg.bytes)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
+
+    msg = AllLinkRecordResponse(
         flags=0x00, group=0x01, address='4d5e6f',
         linkdata1=0x01, linkdata2=0x0b, linkdata3=0x000050)
     plm.data_received(msg.bytes)
-    yield from asyncio.sleep(.1)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
 
-    log.info('Replying with Last All-Link Record')
-    log.info('__________________________________')
-    msg = insteonplm.messages.getNextAllLinkRecord.GetNextAllLinkRecord(0x15)
+    _LOGGER.info('Replying with Last All-Link Record')
+    _LOGGER.info('__________________________________')
+    cmd_sent = yield from wait_for_plm_command(plm, GetNextAllLinkRecord(),
+                                               loop)
+    if not cmd_sent:
+        assert False
+    msg = GetNextAllLinkRecord(MESSAGE_NAK)
     plm.data_received(msg.bytes)
-    yield from asyncio.sleep(.1)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
 
-    log.info('Replying with Device Info Record')
-    log.info('________________________________')
-    msg = insteonplm.messages.standardReceive.StandardReceive(
+    _LOGGER.info('Replying with Device Info Record')
+    _LOGGER.info('________________________________')
+    msg = StandardSend('4d5e6f', COMMAND_ID_REQUEST_0X10_0X00)
+    cmd_sent = wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
+    msg = StandardSend('4d5e6f', COMMAND_ID_REQUEST_0X10_0X00,
+                       acknak=MESSAGE_ACK)
+    plm.data_received(msg.bytes)
+    yield from asyncio.sleep(RECV_MSG_WAIT, loop=loop)
+    msg = StandardReceive(
         address='4d5e6f', target='010b00',
         commandtuple={'cmd1': 0x01, 'cmd2': 0x00},
         flags=MESSAGE_FLAG_BROADCAST_0X80)
     plm.data_received(msg.bytes)
-    yield from asyncio.sleep(.1)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
     for addr in plm.devices:
-        log.info('Device: %s', addr)
+        _LOGGER.info('Device: %s', addr)
 
-    log.info('Replying with Device Status Record')
-    log.info('__________________________________')
-    plm.devices['4d5e6f'].states[0x01].async_refresh_state()
-    yield from asyncio.sleep(.1)
-    msg = insteonplm.messages.standardSend.StandardSend(
-        address='4d5e6f', commandtuple={'cmd1': 0x19, 'cmd2': 0x00},
-        flags=0x00, acknak=0x06)
+    _LOGGER.info('Replying with Device Status Record')
+    _LOGGER.info('__________________________________')
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_STATUS_REQUEST_0X19_0X00)
+    cmd_sent = yield from wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_STATUS_REQUEST_0X19_0X00,
+                       acknak=MESSAGE_ACK)
     plm.data_received(msg.bytes)
-    yield from asyncio.sleep(.1)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
+
     msg = insteonplm.messages.standardReceive.StandardReceive(
         address='4d5e6f', target='1a2b3c',
-        commandtuple={'cmd1': 0x17, 'cmd2': 0xff},
+        commandtuple={'cmd1': 0x17, 'cmd2': 0xaa},
         flags=0x20)
     plm.data_received(msg.bytes)
-    yield from asyncio.sleep(.1)
+    yield from asyncio.sleep(RECV_MSG_WAIT)
 
-    assert plm.devices['4d5e6f'].states[0x01].value == 0xff
+    assert plm.devices['4d5e6f'].states[0x01].value == 0xaa
 
-    # Too much backlog of messages at this point to be clear on
-    # what message will be
-    # returned.
+    _LOGGER.info('Testing device message ACK/NAK handling')
+    _LOGGER.info('_______________________________________')
+    plm.devices['4d5e6f'].states[0x01].on()
+    plm.devices['4d5e6f'].states[0x01].off()
+    plm.devices['4d5e6f'].states[0x01].set_level(0xbb)
 
-    # log.info('Replying with NAK Record')
-    # log.info('__________________________________')
-    # msg = insteonplm.messages.standardSend.StandardSend(
-    #     address='4d5e6f', commandtuple={'cmd1': 0x011,'cmd2': 0xff},
-    #     flags=0x00, acknak=0x15)
-    # plm.data_received(msg.bytes)
-    # yield from asyncio.sleep(3)
-    # assert plm.transport.lastmessage == msg.bytes[:-1]
+    # Test that the first ON command is sent
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_ON_0X11_NONE, cmd2=0xff)
+    cmd_sent = yield from wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
+
+    # ACK the ON command
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_ON_0X11_NONE,
+                       cmd2=0xff, flags=0x00, acknak=MESSAGE_ACK)
+    plm.data_received(msg.bytes)
+
+    # Let the Direct ACK timeout expire
+    yield from asyncio.sleep(DIRECT_ACK_WAIT_TIMEOUT)
+
+    # Test that the Off command has now sent
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_OFF_0X13_0X00)
+    cmd_sent = yield from wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
+
+    # NAK the OFF command and test that the OFF command is resent
+    plm.transport.lastmessage = ''
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_OFF_0X13_0X00,
+                       acknak=MESSAGE_NAK)
+    plm.data_received(msg.bytes)
+
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_OFF_0X13_0X00)
+    cmd_sent = yield from wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
+
+    # Let the ACK/NAK timeout expire and test that the OFF command is resent
+    plm.transport.lastmessage = ''
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_OFF_0X13_0X00)
+    cmd_sent = yield from wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
+
+    # ACK the OFF command and let the Direct ACK message expire
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_OFF_0X13_0X00,
+                       acknak=MESSAGE_ACK)
+    plm.data_received(msg.bytes)
+
+    # Test that the second SET_LEVEL command is sent
+    msg = StandardSend('4d5e6f', COMMAND_LIGHT_ON_0X11_NONE, cmd2=0xbb)
+    cmd_sent = yield from wait_for_plm_command(plm, msg, loop)
+    if not cmd_sent:
+        assert False
 
 
 def test_plm():
     """Main test for the PLM."""
-    devicelist = (
-        {
-            "address": "3c4fc5",
-            "cat": 0x01,
-            "subcat": 0x0b,
-            "product_key": 0x000050
-        },
-        {
-            "address": "43af9b",
-            "cat": 0x02,
-            "subcat": 0x1a,
-            "product_key": 0x00
-        }
-    )
-
-    log = logging.getLogger(__name__)
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(do_plm(loop, log, devicelist))
+    loop.run_until_complete(do_plm(loop))
