6f81d509dd3ffd15758ed3f7950364cc1f0a3983
Tom Harris <tharris@harrisnj.net>
2018-05-31 20:52:43 -0400

    Fix recursive issue

--- insteonplm/devices/x10.py
+++ insteonplm/devices/x10.py
@@ -67,8 +67,9 @@ class X10AllUnitsOff(X10Device):
 
     def _reset_state(self, addr, name, val):
         loop = self._plm.loop
-        asyncio.wait(asyncio.sleep(.5, loop=loop), loop=loop)
-        self._stateList[0x01].reset()
+        if val == 0x00:
+            asyncio.wait(asyncio.sleep(.5, loop=loop), loop=loop)
+            self._stateList[0x01].reset()
 
     def _register_messages(self):
         all_units_off_msg = X10Received.command_msg(self.address.x10_housecode,
@@ -93,8 +94,9 @@ class X10AllLightsOn(X10Device):
 
     def _reset_state(self, addr, name, val):
         loop = self._plm.loop
-        asyncio.wait(asyncio.sleep(.5, loop=loop), loop=loop)
-        self._stateList[0x01].reset()
+        if val == 0xff:
+            asyncio.wait(asyncio.sleep(.5, loop=loop), loop=loop)
+            self._stateList[0x01].reset()
 
     def _register_messages(self):
         all_on_msg = X10Received.command_msg(self.address.x10_housecode,
@@ -119,8 +121,9 @@ class X10AllLightsOff(X10Device):
 
     def _reset_state(self, addr, name, val):
         loop = self._plm.loop
-        asyncio.wait(asyncio.sleep(.5, loop=loop), loop=loop)
-        self._stateList[0x01].reset()
+        if val == 0x00:
+            asyncio.wait(asyncio.sleep(.5, loop=loop), loop=loop)
+            self._stateList[0x01].reset()
 
     def _register_messages(self):
         all_off_msg = X10Received.command_msg(self.address.x10_housecode,
