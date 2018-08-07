08aaff81bb93c172b4468451bb3bf6ff855640f4
Tom Harris <tharris@harrisnj.net>
2018-08-06 23:18:50 -0400

    Bump version to 0.12.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if len(long_description) < 100:
 
 setup(
     name='insteonplm',
-    version='0.11.7',
+    version='0.12.0',
     author='David McNett',
     author_email='nugget@macnugget.org',
     url='https://github.com/nugget/python-insteonplm',
