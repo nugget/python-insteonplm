aa0089fd7f6382b2148341ceda0f2e691b2cf869
Tom Harris <tharris@harrisnj.net>
2019-07-30 16:45:47 -0400

    Scene cleanup (#173)
    * Send group number in cmd2 with cleanup message
    
    * Bump version to 0.16.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name='insteonplm',
-    version='0.16.4',
+    version='0.16.5',
     author='David McNett',
     author_email='nugget@macnugget.org',
     url='https://github.com/nugget/python-insteonplm',
